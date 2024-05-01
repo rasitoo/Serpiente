85bb1288fbdb1853366ea28eaefea1021634d484
Rodrigo <05rotapi@gmail.com>
2024-05-01 12:14:14 +0200

    añadido main y experimentos

---
+++ src/main/java/ui/canva/Main.java
@@ -0,0 +1,11 @@
+package ui.canva;
+
+/**
+ * @author Rodrigo
+ * @date 01 mayo, 2024
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/ui/canva/Tickable.java
+++ src/main/java/ui/canva/Tickable.java
@@ -2,8 +2,8 @@ package ui.canva;
 // Un objeto que sea "Tickable" es un objeto que est� interesado
 // en saber c�mo van pasando unidades de tiempo.
 // Por CADA UNA de estas unidades de tiempo, yo tengo la
-// obligaci�n de llamar a su m�todo tick().
-// El objeto, a su vez, actualizar� su estado interno en el m�todo
+// obligaci�n de llamar a su metodo tick().
+// El objeto, a su vez, actualizar� su estado interno en el metodo
 // (ubicaci�n//movimiento, puntuaciones, etc.).
 
 public interface Tickable {
