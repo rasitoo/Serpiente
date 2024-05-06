9a253eb4e02497f4e797e04c0d23196936402820
patri <parosa2005@gmail.com>
2024-05-06 23:51:31 +0200

    vuelta al menu de antes y al darle a facil se abre la ventana del juego de la serpiente

--- src/main/java/ui/menu/MenuPrincipal.java
+++ src/main/java/ui/menu/MenuPrincipal.java
@@ -1,5 +1,8 @@
 package ui.menu;
 
+import ui.canva.Serpiente;
+import ui.canva.Vista;
+
 import javax.swing.*;
 import javax.swing.border.MatteBorder;
 import java.awt.*;
@@ -10,83 +13,21 @@ import static java.lang.Thread.sleep;
 
 public class MenuPrincipal extends javax.swing.JFrame{
 
-    public MenuPrincipal() {
-        initComponents();
-
-
-
-        NewJFrame1 y = new NewJFrame1();
-        y.setVisible(true);
-        y.setBounds(0, 0, 800, 800);
-
-        this.add(y);
-
-        NewJFrame x = new NewJFrame();
-        x.setVisible(true);
-        x.setBounds(0, 0, 800, 800);
-
-        this.add(x);
-
-    }
-
-    private void initComponents() {
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setMaximumSize(new java.awt.Dimension(474, 263));
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 399, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGap(0, 300, Short.MAX_VALUE)
-        );
-
-        pack();
-    }
 
 
     public static void main(String[] args) {
-
-        {
-            /* Set the Nimbus look and feel */
-            //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-            /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-             * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
-             */
-            try {
-                for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                    if ("Nimbus".equals(info.getName())) {
-                        javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                        break;
-                    }
-                }
-            } catch (ClassNotFoundException ex) {
-                java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-            } catch (InstantiationException ex) {
-                java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-            } catch (IllegalAccessException ex) {
-                java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-            } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-                java.util.logging.Logger.getLogger(Main.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-            }
-            //</editor-fold>
-
-            /* Create and display the form */
-            java.awt.EventQueue.invokeLater(new Runnable() {
-                @Override
-                public void run() {
-                    new Main().setVisible(true);
-                }
-            });
-        }
+        int maxGap=20;
 
         JFrame ventana= new JFrame ("Menú");
         ventana.setPreferredSize(new Dimension(500,320));
         ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        ImageIcon imagenFondo = new ImageIcon("C:\\Users\\paros\\IdeaProjects\\Serpiente\\src\\main\\java\\imagenes\\fondo3.png");
+        JLabel fondo = new JLabel(imagenFondo);
+        fondo.setSize(ventana.getContentPane().getSize());
+        fondo.setOpaque(true);
+        ventana.getContentPane().add(fondo, BorderLayout.CENTER);
+
 
         /*Estilo del cursor*/
         Toolkit tk = Toolkit.getDefaultToolkit();
@@ -94,8 +35,60 @@ public class MenuPrincipal extends javax.swing.JFrame{
         Cursor cursor = tk.createCustomCursor(icon.getImage(), new Point(0, 0), "Cursor personalizado");
         ventana.setCursor(cursor);
 
+        JButton boton = new JButton("Jugar");
+        boton.setPreferredSize(new Dimension(0,60));
+        boton.setForeground(Color.BLACK);
+        boton.setFont(new Font("Calibri", Font.BOLD, 28));
+        boton.setBorder(BorderFactory.createMatteBorder(3, 3, 2, 2, Color.BLACK));
+
+        boton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                JFrame ventanaDificultades= new JFrame ("Niveles");
+                ventanaDificultades.setCursor(cursor);
+                ventana.setVisible(false);
+                ventanaDificultades.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                ventanaDificultades.setPreferredSize(new Dimension(500,320));
+
+                JLabel fondo = new JLabel(new ImageIcon("C:\\Users\\paros\\IdeaProjects\\Serpiente\\src\\main\\java\\imagenes\\fondo2.jpeg"));
+                fondo.setSize(ventanaDificultades.getContentPane().getSize());
+                fondo.setOpaque(true);
+
+                ventanaDificultades.getContentPane().add(fondo, BorderLayout.CENTER);
+
+                JButton boton1=new JButton("  Fácil  ");
+                JButton boton2=new JButton(" Difícil ");
+                boton1.setForeground(Color.BLACK);
+                boton2.setForeground(Color.BLACK);
+                boton1.setFont(new Font("Calibri", Font.BOLD, 28));
+                boton2.setFont(new Font("Calibri", Font.BOLD, 28));
+                boton1.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.BLACK));
+                boton2.setBorder(BorderFactory.createMatteBorder(2, 2, 2, 2, Color.BLACK));
+
+                boton1.addActionListener(new ActionListener() {
+                    public void actionPerformed(ActionEvent e2) {
+                        ventanaDificultades.setVisible(false);
+                        Vista v=new Vista("Juego de la serpiente", 800, 800);
+                        v.setVisible(true);
+                        v.setResizable(false);
+                    }
+                });
+
+                ventanaDificultades.getContentPane().add(boton1, BorderLayout.NORTH);
+                boton1.setPreferredSize(new Dimension(0,50));
+                ventanaDificultades.getContentPane().add(boton2, BorderLayout.SOUTH);
+                boton2.setPreferredSize(new Dimension(0,50));
+                ventanaDificultades.setResizable(false);
+                ventanaDificultades.pack();
+                ventanaDificultades.setVisible(true);
+            }
+        });
+        ventana.getContentPane().add(boton, BorderLayout.SOUTH);
         ventana.setResizable(false); //para no poder modificar el tamaño de la pantalla
         ventana.pack();
+        ventana.setVisible(true);
         System.out.println("Se muestra la ventana ");
+
     }
 }
